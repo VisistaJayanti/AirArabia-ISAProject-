58252ed78836699ddf66674ef7b26eda04981507
VisistaJayanti <130677795+VisistaJayanti@users.noreply.github.com>
2024-08-01 14:27:09 +0400

    Update ApplicationLoginApplication.java

--- ApplicationLogin/ApplicationLogin/src/main/java/com/example/ApplicationLogin/ApplicationLoginApplication.java
+++ ApplicationLogin/ApplicationLogin/src/main/java/com/example/ApplicationLogin/ApplicationLoginApplication.java
@@ -1,11 +1,17 @@
 package com.example.ApplicationLogin;
 
 import com.example.ApplicationLogin.Service.LoginService;
+import com.example.ApplicationLogin.Service.impl.User_Impl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
@@ -23,6 +29,22 @@ public class ApplicationLoginApplication {
 	@Bean
 	BCryptPasswordEncoder bCryptPasswordEncoder(){
 		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	private User_Impl userDetailsService;
+	//
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Bean
+	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+		AuthenticationManagerBuilder authenticationManagerBuilder =
+				http.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder
+				.userDetailsService(userDetailsService)
+				.passwordEncoder(bCryptPasswordEncoder);
+		return authenticationManagerBuilder.build();
 	}
 
 	//creating a bean
