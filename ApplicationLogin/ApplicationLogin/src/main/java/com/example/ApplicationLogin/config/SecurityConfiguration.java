8c743e57ee2f50ea52ac1a9c223200f15bd9650a
VisistaJayanti <130677795+VisistaJayanti@users.noreply.github.com>
2024-08-01 14:24:43 +0400

    Update SecurityConfiguration.java

--- ApplicationLogin/ApplicationLogin/src/main/java/com/example/ApplicationLogin/config/SecurityConfiguration.java
+++ ApplicationLogin/ApplicationLogin/src/main/java/com/example/ApplicationLogin/config/SecurityConfiguration.java
@@ -1,18 +1,17 @@
 package com.example.ApplicationLogin.config;
 
+import com.example.ApplicationLogin.Service.impl.User_Impl;
 import com.example.ApplicationLogin.filter.CustomAuthenticationFilter;
 import com.example.ApplicationLogin.filter.CustomAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -21,48 +20,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfiguration {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private User_Impl userDetailsService;
     //
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public SecurityConfiguration(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userDetailsService = userDetailsService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder);
-        return authenticationManagerBuilder.build();
-    }
+    AuthenticationManager  authenticationManagerx;
 
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().permitAll()
-//                );
-//        return http.build();
-//    }
-
+    //This is the authentication part
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // CustomAuthenticationFilter handles the login and token generation
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager(http));
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerx);
         customAuthenticationFilter.setFilterProcessesUrl("/api/v1/user/login");
 
         // CSRF protection is disabled since this is a stateless API (JWT based)
@@ -71,8 +42,8 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         // Public endpoints (no authentication required
-                                .requestMatchers("*").permitAll()
-//                        .requestMatchers("/api/v1/user/save", "/api/v1/user/login", "/api/token/refresh").permitAll()
+//                                .requestMatchers("*").permitAll()
+                        .requestMatchers("/api/v1/user/save", "/api/v1/user/login", "/api/token/refresh").permitAll()
                         // Secured endpoints (authentication and specific roles required)
                         .requestMatchers("/api/v1/user").hasAnyAuthority("ROLE_USER")
                         .requestMatchers("/api/v1/user/save").hasAnyAuthority("ROLE_ADMIN")
@@ -85,9 +56,6 @@ public class SecurityConfiguration {
         return http.build();
     }
 }
-
-
-
 
 
 
